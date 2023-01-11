a35d6d3833557bfcb56dc2c35cc523fa5ea5ecb3
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-01-11 05:36:53 +0100

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Laravel 9 Crud Application</title>
+</head>
+<body>
+   <div class="container mt-5">
+ <h1 class="text-primary mt-3 mb-4 text-center"><b>Laravel 9 Crud Application -load My sql Data in HTML table with pagination</b></h1>
+@yield('content') 
+
+   </div> 
+</body>
+</html>
